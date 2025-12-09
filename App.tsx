7f4e749061b2fd7250f7949456f517cfe4256fe6
kevinolm10 <kevinolm10@gmail.com>
2025-12-09 21:38:47 +0100

    Query random value to the API fetching and link it to a button so the user fetches random pokemon each time

--- App.tsx
+++ App.tsx
@@ -1,32 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
-import { useEffect } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, Button, Image } from 'react-native';
 
-
+type Pokemon = {
+  name?: string;
+  image?: string;
+};
 
 export default function App() {
+  const [pokeData, setPokeData] = useState<Pokemon>({});
 
-useEffect(() => {
-  fetch("http://192.168.0.161:5000/")
-  .then(response => {
+  function getData() {
+    const min = 1;
+    const max = 300;
+    const randPoke = Math.floor(Math.random() * (max - min + 1)) + min;
+
+    fetch(`http://192.168.0.161:5000/${randPoke}`)
+      .then(response => {
         if (!response.ok) throw new Error("Error fetching data");
         return response.json();
       })
-      .catch(error => console.log(error))
-})
+      .then(data => {
+        setPokeData({ 
+          name: data.name, 
+          image: data.sprites.front_default
+        });
+      })
+      .catch(error => console.log(error));
+  }
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  });
 
   return (
     <View style={styles.container}>
-      <Text>Hello World</Text>
+      <Button title="Randomizer" onPress={getData} />
+
+      {pokeData.name && (
+        <View style={{ alignItems: "center", marginTop: 20 }}>
+          <Text>{`${pokeData.name}`}</Text>
+          {pokeData.image && (
+            <Image
+              source={{ uri: pokeData.image }}
+              style={{ width: 100, height: 100 }}
+            />
+          )}
+        </View>
+      )}
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
